0b6bb1845dd621f91ace75d01f6625d96b28727d
OnlineTrainer <mohamedtechdata@gmail.com>
2022-10-04 06:56:54 +0530

    04-10-2022

---
+++ AdvanceJavaP2/src/main/java/Servlet1.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet 
+{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		PrintWriter out=resp.getWriter();
+		
+		String uname=req.getParameter("uname");
+		String pass=req.getParameter("pass");
+		
+		if(uname.equals("admin")&&pass.equals("admin@123"))
+		{
+			RequestDispatcher rd=req.getRequestDispatcher("AdminHomePage.jsp");
+			rd.forward(req, resp);
+			//out.println("LoginSucess!!!");
+		}
+		else
+		{
+			RequestDispatcher rd=req.getRequestDispatcher("error.jsp");
+			rd.include(req, resp);
+			//out.println("LoginFail!!!");
+		}
+		
+		
+		//out.println("Your data is::"+uname+""+pass);
+		
+		
+		
+	}
+
+}

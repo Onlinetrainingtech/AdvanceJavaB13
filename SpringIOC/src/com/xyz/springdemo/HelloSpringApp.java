7a0c78ee93cdd0deaae8303cc7e5f0c1d053a2fd
OnlineTrainer <mohamedtechdata@gmail.com>
2022-11-29 06:58:10 +0530

    29-11-2022

---
+++ SpringIOC/src/com/xyz/springdemo/HelloSpringApp.java
@@ -0,0 +1,27 @@
+package com.xyz.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		
+	  
+		
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach theCoach=context.getBean("m1",Coach.class);
+		
+		Coach theCoach1=context.getBean("m2",Coach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theCoach1.getDailyWorkout());
+		
+		
+
+		
+		context.close();
+	}
+
+}
